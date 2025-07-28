149bfef96d7e25c559cdfb344e20f3ac7f83e266
Vivek Sharma <sharma@gmail.com>
2025-07-28 20:36:23 +0530

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,21 @@
+"use client";
+import Image from "next/image";
+import styles from "./page.module.css";
+import { checkout } from "./checkout";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <div>
+        <Image alt="illuminati" src="/payment.png" width={500} height={270}/>
+      </div>
+      <button className={styles.button}
+      onClick={(()=>{
+        checkout({
+          lineItems:[{price:"price_1RpmXQ4rv3nB2eYtBTV8i1zR",quantity:1}]
+        })
+      })}
+      >Buy now</button>
+    </main>
+  )
+}
